07b47531da5b7fd6f76d140c670c47784101e468
ladyscream <the.lady.scream@gmail.com>
2019-05-28 08:26:47 -0500

    Changed colors

--- config.def.h
+++ config.def.h
@@ -11,15 +11,16 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Fira Mono:size=10" };
 static const char dmenufont[]       = "Fira Mono:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char normbgcolor[]     = "#88927E";
+static const char normfgcolor[]     = "#ffffff";
+static const char normbordercolor[] = "#88927E";
+static const char selbgcolor[]      = "#DB7464";
+static const char selfgcolor[]      = "#ffffff";
+static const char selbordercolor[]  = "#DB7464";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+	[SchemeSel]  = { selfgcolor, selbgcolor,  selbordercolor  },
 };
 
 /* tagging */
