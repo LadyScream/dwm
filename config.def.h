efafab1a12788424c80e469cf04a0896ca19f0f1
ladyscream <the.lady.scream@gmail.com>
2019-05-30 22:49:17 -0500

    Added prompt for dmenu

--- config.def.h
+++ config.def.h
@@ -61,7 +61,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 /*static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };*/
-static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-dim", "0.2", "-b", "-q", "-i", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-dim", "0.2", "-b", "-q", "-i", "-p", "Run:", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browser[]  = { "chromium", NULL };
 static const char *passmenu[] = { "passmenu", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-dim", "0.2", "-b", "-i", NULL };
